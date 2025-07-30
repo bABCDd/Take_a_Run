ee57b8af6b26b71be323bea1c9f38191b383be64
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-30 16:46:45 +0900

    fix: BgLooper 주석처리

--- Assets/Scripts/BgLooper.cs
+++ Assets/Scripts/BgLooper.cs
@@ -11,17 +11,18 @@ public class BgLooper : MonoBehaviour
     void Start()
     {
         //Obstacle 이라는  컴포넌트를 가진 모든 오브젝트를 찾아서 배열로 저장합니다.
-        Obstacle[] obstacle = GameObject.FindObjectsOfType<Obstacle>();
         // 첫번째 장애물의 위치를 저장합니다. 위에 코드 주석처리 해도 정상작동하네?
         // 지금은 옵스타클 클래스가 없어서 오류가 발생함
-        obstacleLastPosition = obstacle[0].transform.position;
         // 장애물의 개수를 저장합니다.
-        obstacleCount = obstacle.Length;
-
         //장애물 위치를 랜덤하게 설정
+
+        //Obstacle[] obstacle = GameObject.FindObjectsOfType<Obstacle>();
+        //obstacleLastPosition = obstacle[0].transform.position;
+        //obstacleCount = obstacle.Length;
+
         for (int i = 0; i < obstacleCount; i ++)
         {
-            obstacleLastPosition = obstacle[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
+            //obstacleLastPosition = obstacle[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
         }
     }
 
@@ -39,10 +40,10 @@ public class BgLooper : MonoBehaviour
             collision.transform.position = pos;
         }
 
-        Obstacle obstacle = collision.GetComponent<Obstacle>();
-        if(obstacle)
-        {
-            obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
-        }
+        //Obstacle obstacle = collision.GetComponent<Obstacle>();
+        //if(obstacle)
+        //{
+        //    obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
+        //}
     }
 }
