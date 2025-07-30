bb08c3574c83eed1bf0e881d0ebf1ba64b8a89ed
Kseongmin1107 <ksmsky1107@gmail.com>
2025-07-30 19:07:46 +0900

    Update BgLooper.cs

--- Assets/Scripts/BgLooper.cs
+++ Assets/Scripts/BgLooper.cs
@@ -5,29 +5,7 @@ using UnityEngine;
 public class BgLooper : MonoBehaviour
 {
     public int numBgCoont = 5;
-    public int obstacleCount = 0;
-    public Vector3 obstacleLastPosition = Vector3.zero;
 
-    void Start()
-    {
-        //Obstacle 이라는  컴포넌트를 가진 모든 오브젝트를 찾아서 배열로 저장합니다.
-        // 첫번째 장애물의 위치를 저장합니다. 위에 코드 주석처리 해도 정상작동하네?
-        // 지금은 옵스타클 클래스가 없어서 오류가 발생함
-        // 장애물의 개수를 저장합니다.
-        //장애물 위치를 랜덤하게 설정
-
-        //Obstacle[] obstacle = GameObject.FindObjectsOfType<Obstacle>();
-        //obstacleLastPosition = obstacle[0].transform.position;
-        //obstacleCount = obstacle.Length;
-
-        for (int i = 0; i < obstacleCount; i ++)
-        {
-            //obstacleLastPosition = obstacle[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
-        }
-    }
-
-
-    //
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //충돌한 오브젝트가 배경이라면
@@ -39,11 +17,5 @@ public class BgLooper : MonoBehaviour
             pos.x = widthOfBgObject* numBgCoont;
             collision.transform.position = pos;
         }
-
-        //Obstacle obstacle = collision.GetComponent<Obstacle>();
-        //if(obstacle)
-        //{
-        //    obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
-        //}
     }
 }
